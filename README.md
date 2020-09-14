821e56af16c3ef788aa78e1e40ece4b15436899b
Jason Chung <jason.chung@skylinetw.com>
2020-09-14 18:39:04 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# @0y0/eslint-config · [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/o0y0o/eslint-config/blob/master/LICENSE) [![npm](https://img.shields.io/npm/v/@0y0/eslint-config.svg)](https://www.npmjs.com/package/@0y0/eslint-config) ![Status](https://github.com/o0y0o/eslint-config/workflows/Package/badge.svg)
+
+Common ESLint configs.
+
+## License
+
+[MIT](https://github.com/o0y0o/eslint-config/blob/master/LICENSE)
---
+++ packages/ vanilla/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['standard', 'prettier'],
+  plugins: ['import'],
+  rules: {
+    'import/order': 'error',
+    'no-console': 'error',
+    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    'no-var': 'error',
+    quotes: ['error', 'single', { avoidEscape: true }]
+  },
+  overrides: [
+    {
+      files: '**/*.{test,spec}.js',
+      env: { jest: true, mocha: true },
+      rules: {
+        'no-unused-expressions': 'off'
+      }
+    }
+  ]
+}
