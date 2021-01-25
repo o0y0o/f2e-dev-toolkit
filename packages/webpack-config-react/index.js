d293b13ed85e4325418627ba14c22988178c5441
Jason Chung <shiningjason1989@gmail.com>
2021-01-25 12:52:17 +0800

    refactor: update webpack config (#34)
    Co-authored-by: Jason Chung <jason.chung@skylinetw.com>

--- packages/webpack-config-react/index.js
+++ packages/webpack-config-react/index.js
@@ -1,65 +1,10 @@
-const path = require('path')
-const getDevServerConfig = require('./lib/getDevServerConfig')
-const getDevtoolConfig = require('./lib/getDevtoolConfig')
-const getModuleConfig = require('./lib/getModuleConfig')
-const getOptimizationConfig = require('./lib/getOptimizationConfig')
-const getOutputConfig = require('./lib/getOutputConfig')
-const getPluginConfig = require('./lib/getPluginConfig')
-const getResolveConfig = require('./lib/getResolveConfig')
-const getStatsConfig = require('./lib/getStatsConfig')
+const createConfig = require('@0y0/webpack-config-vanilla')
+const getOptions = require('@0y0/webpack-config-vanilla/lib/getOptions')
+const setReactRefreshConfig = require('./lib/setReactRefreshConfig')
 
-module.exports = function ({
-  isDev = true,
-  appName,
-  rootDir,
-  srcDir = path.join(rootDir, 'src'),
-  distDir = path.join(rootDir, 'dist'),
-  staticDir = path.join(srcDir, 'static'),
-  jsChunks = { app: path.join(srcDir, 'index.js') },
-  htmlChunks = { app: path.join(srcDir, 'index.html') },
-  publicPath = '/',
-  assetPath = '/',
-  serviceWorkerPath = '/sw.js',
-  resolveAlias,
-  chunkGroups,
-  variables = {},
-  workboxOptions,
-  useSourceMap = false,
-  useWebpackAnalyzer = false,
-  host = 'localhost',
-  port = 8080,
-  proxy
-}) {
-  publicPath = `${publicPath}/`
-    .replace(/^(?!\w+:\/\/)/, '/')
-    .replace(/\/+/g, '/')
-  assetPath = `${assetPath}/`
-    .replace(/\/+/g, '/')
-    .replace(/^\//, '')
-    .replace(/([^/])$/, '$1/')
-  return {
-    mode: isDev ? 'development' : 'production',
-    stats: getStatsConfig(),
-    bail: !isDev,
-    devtool: getDevtoolConfig({ isDev, useSourceMap }),
-    entry: jsChunks,
-    output: getOutputConfig({ isDev, distDir, publicPath, assetPath }),
-    optimization: getOptimizationConfig({ isDev, chunkGroups, useSourceMap }),
-    resolve: getResolveConfig({ resolveAlias }),
-    module: getModuleConfig({ isDev, srcDir, useSourceMap }),
-    plugins: getPluginConfig({
-      isDev,
-      appName,
-      rootDir,
-      staticDir,
-      htmlChunks,
-      publicPath,
-      assetPath,
-      serviceWorkerPath,
-      variables,
-      workboxOptions,
-      useWebpackAnalyzer
-    }),
-    devServer: getDevServerConfig({ publicPath, host, port, proxy })
-  }
+module.exports = function (parameters) {
+  const options = getOptions(parameters)
+  const config = createConfig(options)
+  if (options.isDev) setReactRefreshConfig(config)
+  return config
 }
--- packages/webpack-config-react/lib/getDevtoolConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = function ({ isDev, useSourceMap }) {
-  return isDev
-    ? 'eval-cheap-module-source-map'
-    : useSourceMap
-    ? 'source-map'
-    : false
-}
